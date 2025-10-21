8a57ad60e98fcc83a37eb1031b108f5423315894
renpanlong <506226120@qq.com>
2025-10-21 14:27:08 +0800

    Create start_all.py

---
+++ start_all.py
@@ -0,0 +1,13 @@
+import os
+
+def start_proxies_lists():
+    os.system('C:\\Users\\S1127809\\Anaconda3\\python.exe 1.66ip.py && 2.kuaidaili.py'
+              # C:\\Users\\S1127809\\Anaconda3\\python.exe 2.kuaidaili.py &&
+              # C:\\Users\\S1127809\\Anaconda3\\python.exe 3.89IP.py &&
+              # C:\\Users\\S1127809\\Anaconda3\\python.exe 4.jiangxianli.py &&
+              # C:\\Users\\S1127809\\Anaconda3\\python.exe 5.IP3366.py &&
+              # C:\\Users\\S1127809\\Anaconda3\\python.exe 6.kxdaili.py'''
+              )
+
+
+start_proxies_lists()
